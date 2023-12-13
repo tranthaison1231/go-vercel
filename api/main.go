531486cc085488cdd7719f61f46dd377e85ea4f2
son.tran <son.tran@team.enouvo.com>
2023-12-13 11:42:57 +0700

    feat(master): add go project

--- api/main.go
+++ api/main.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +20,16 @@ func initRoutes(r *gin.RouterGroup) {
 
 func init() {
 	app = gin.New()
+
+	app.NoRoute(func(c *gin.Context) {
+		sb := &strings.Builder{}
+		sb.WriteString("routing err: no route, try this:\n")
+		for _, v := range app.Routes() {
+			sb.WriteString(fmt.Sprintf("%s %s\n", v.Method, v.Path))
+		}
+		c.String(http.StatusBadRequest, sb.String())
+	})
+
 	r := app.Group("/api")
 	initRoutes(r)
 }
