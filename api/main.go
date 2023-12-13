2344da73e962c293fc9d9febceebf3272c05b7b0
son.tran <son.tran@team.enouvo.com>
2023-12-13 11:36:27 +0700

    feat(master): add go project

---
+++ api/main.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	app *gin.Engine
+)
+
+func initRoutes(r *gin.RouterGroup) {
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+}
+
+func init() {
+	app = gin.New()
+	r := app.Group("/api")
+	initRoutes(r)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
+}
